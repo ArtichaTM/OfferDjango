5d1cba1b08ef4a3bed65499e3799f2dce1010329
Articha <tima-1324@mail.ru>
2026-02-28 21:22:13 +0300

    Added custom column name

--- about/admin.py
+++ about/admin.py
@@ -8,8 +8,16 @@ from . import models
 
 @admin.register(models.AboutImages)
 class AboutImagesAdmin(SortableAdminMixin, admin.ModelAdmin):
-    list_display = ['logo_preview', 'logo', 'order']
+    list_display = ['logo_preview', 'custom_logo_display', 'order']
+    list_display_links = ['logo_preview']
 
     def logo_preview(self, obj):
         return format_html('<img src="{}" />', obj.logo.thumbnails['admin_clipboard_icon'])
+
+    
+    def custom_logo_display(self, obj):
+        return obj.logo.label
+
     logo_preview.short_description = "Предпросмотр"
+    custom_logo_display.admin_order_field = 'logo'
+    custom_logo_display.short_description = 'Название'
