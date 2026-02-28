08409d97711c38346894fc3fe834a1096751a373
Articha <tima-1324@mail.ru>
2026-02-28 21:35:11 +0300

    Added removing images when user deletes them (`FilerImageField` is a ForeignKey)

--- about/models.py
+++ about/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_delete
 
 from filer.fields.image import FilerImageField
 
@@ -21,3 +23,8 @@ class AboutImages(models.Model):
         ordering = ['-order']
         verbose_name = "Изображение"
         verbose_name_plural = "Изображения"
+
+
+@receiver(post_delete, sender=AboutImages)
+def mymodel_post_delete(sender, instance: AboutImages, using, **kwargs):
+    instance.logo.delete()
