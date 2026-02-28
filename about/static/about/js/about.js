8b923a97e78324ab6a6f2882427762980f90fb48
Articha <tima-1324@mail.ru>
2026-02-28 19:04:32 +0300

    Improved `about.js` with ready function

--- about/static/about/js/about.js
+++ about/static/about/js/about.js
@@ -1,1 +1,11 @@
-$('.slickable').slick();
+$(document).ready(function(){
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    arrows: true
+  });
+});
