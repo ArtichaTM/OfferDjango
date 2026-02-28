f4f2283fd6e7395715fb758da82e734d50677e98
Articha <tima-1324@mail.ru>
2026-02-28 21:29:26 +0300

    Improved displaying of all admin

--- about/admin.py
+++ about/admin.py
@@ -8,11 +8,12 @@ from . import models
 
 @admin.register(models.AboutImages)
 class AboutImagesAdmin(SortableAdminMixin, admin.ModelAdmin):
-    list_display = ['logo_preview', 'custom_logo_display', 'order']
-    list_display_links = ['logo_preview']
+    list_display = ['order', 'logo_preview', 'custom_logo_display']
+    list_display_links = ['custom_logo_display']
 
     def logo_preview(self, obj):
-        return format_html('<img src="{}" />', obj.logo.thumbnails['admin_clipboard_icon'])
+        print(obj.logo.thumbnails)
+        return format_html('<img src="{}" />', obj.logo.thumbnails['admin_directory_listing_icon'])
 
     
     def custom_logo_display(self, obj):
@@ -21,3 +22,8 @@ class AboutImagesAdmin(SortableAdminMixin, admin.ModelAdmin):
     logo_preview.short_description = "Предпросмотр"
     custom_logo_display.admin_order_field = 'logo'
     custom_logo_display.short_description = 'Название'
+
+    class Media:
+        css = {
+            'all': ('about/css/admin.css',)
+        }
